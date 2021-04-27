241d9f4a579e120d3c58ca61b6c9ce368daef074
Catherine <kagawor@gmail.com>
2021-04-27 07:53:12 +0200

    COMMIT 04/27 - iterfejs użytkownika nieco poprawiona wersja

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,34 +1,40 @@
-import java.time.LocalDate;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 public class User {
-    // wybór wyszukiwania
-    System.out.println("Po czym chcesz wyszukać: 1 -> Po reginie, 2 -> Po danych geograicznych");
-    Scanner scan = new Scanner(System.in);
-    int choice = scan.nextInt();
-
-    // wyszukiwanie po rejonie
-    if (choice == 1){
-        System.out.println("Podaj miejscowość");
+    public static void main(String[] args) {
+        // wybór wyszukiwania
+        System.out.println("Po czym chcesz wyszukać: 1 -> Po reginie, 2 -> Po danych geograicznych");
         Scanner scan = new Scanner(System.in);
-        String region = scan.nextLine();
-    }
-    // wyszukiwanie po danych geograficznych
-    else if (choice == 2){
-        System.out.println("Podaj długość geograficzną");
-        Scanner scan = new Scanner(System.in);
-        String longitude = scan.nextLine();
-        System.out.println("Podaj szerokość geograficzną");
-        Scanner scan = new Scanner(System.in);
-        String latitude = scan.nextLine();
-    }
+        int choice = scan.nextInt();
 
-    // inne
-    else{
-        System.out.println("podana wartość jest nie poprawna");
-    }
+        // wyszukiwanie po rejonie
+        if (choice == 1) {
+            System.out.println("Podaj miejscowość");
+            Scanner scan2 = new Scanner(System.in);
+            String region = scan.nextLine();
+        }
+        // wyszukiwanie po danych geograficznych
+        else if (choice == 2) {
+            System.out.println("Podaj długość geograficzną"); //podać jak użytkownik ma podawać wartości
+            Scanner scan3 = new Scanner(System.in);
+            String longitude = scan3.nextLine();
+            System.out.println("Podaj szerokość geograficzną");
+            Scanner scan4 = new Scanner(System.in);
+            String latitude = scan4.nextLine();
+        }
 
-    // data
-    System.out.println("Podaj date");
-    Scanner scan = new Scanner(System.in);
-    LocalDate date = scan.nextLine();
+        // inne
+        else {
+            System.out.println("Podana wartość jest nie poprawna");
+        }
+
+        // data
+        System.out.println("Podaj date (dd, MM, yyyy)");
+        Scanner input = new Scanner(System.in);
+        String  date = input.nextLine();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd, MM, yyyy");
+        String date1String = date;
+        System.out.println(date);
+    }
 }
