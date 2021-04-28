fe7092bf83d11f1b759b254108fd268cb04b5f71
Grzebere <68393051+Grzebere@users.noreply.github.com>
2021-04-28 19:09:23 +0200

    COMMIT 4/27 - Dadanie metod SELECT i INSERT dla komunikacji z bazą danych

---
+++ src/main/java/weather/weatherServices/WeatherFunctions.java
@@ -0,0 +1,34 @@
+package weather.weatherServices;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class WeatherFunctions {
+
+    public static String GetWeatherByCity(String region) throws IOException {
+        final String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=" + region + "&units=metric&appid=ed427a111d64798753394af633106470";
+        Request request = new Request.Builder()
+                .url(BASE_URL)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+        Response response = call.execute();
+        return response.body().string();
+    }
+
+    public static String GetWeatherByCoordinates(String longitude, String latitude) throws IOException {
+        final String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&appid=ed427a111d64798753394af633106470";
+        Request request = new Request.Builder()
+                .url(BASE_URL)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+        Response response = call.execute();
+        return response.body().string();
+    }
+
+}
